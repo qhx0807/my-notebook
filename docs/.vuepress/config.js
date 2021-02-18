036c6acff808b657ac78cbf9fcfb7fccb3449ce3
qhx0807 <820240134@qq.com>
2021-02-18 16:02:46 +0800

    update vuepress

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,30 +9,36 @@ module.exports = {
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }]
   ],
+  plugins: {
+    "vuepress-plugin-auto-sidebar": {}
+  },
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
-      { text: '基础知识', link: '/guide/prototype' },
-      { text: '必会习题', link: '/questions/' },
+      { text: '基础知识', link: '/guide/' },
+      { text: '经典题目', link: '/questions/' },
+      { text: '性能优化', link: '/performance/' },
+      { text: '算法', link: '/algorithm/' },
       { text: 'Github', link: 'https://github.com/qhx0807' },
     ],
     sidebarDepth: 1,
-    sidebar: {
-      '/guide/': [
-        {
-          title: 'JavaScript基础',
-          path: '/guide',
-          collapsable: false,
-          children: [
-            '/guide/prototype',
-            '/guide/context-and-stack',
-            '/guide/scope-and-scope-chain'
-          ]
-        },
-      ],
-      '/questions/': [
-        ''
-      ]
-    }
+    sidebar: 'auto'
+    // {
+    //   '/guide/': [
+    //     {
+    //       title: 'JavaScript基础',
+    //       path: '/guide',
+    //       collapsable: false,
+    //       children: [
+    //         '/guide/prototype',
+    //         '/guide/context-and-stack',
+    //         '/guide/scope-and-scope-chain'
+    //       ]
+    //     },
+    //   ],
+    //   '/questions/': [
+    //     ''
+    //   ]
+    // }
   }
 }
